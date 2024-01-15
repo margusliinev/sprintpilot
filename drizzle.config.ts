faa3f642393a9ccb7bfd20dd7ecaf8c12f2f23af
Margus Liinev <margus.liinev@gmail.com>
2024-01-15 19:49:49 +0200

    updated configs, scripts, env and added base db schema with users and sessions

---
+++ drizzle.config.ts
@@ -0,0 +1,10 @@
+import { type Config } from 'drizzle-kit';
+
+import { env } from '@/env';
+
+export default {
+    schema: './src/server/db/schema.ts',
+    driver: 'mysql2',
+    dbCredentials: { uri: env.DATABASE_URL },
+    tablesFilter: ['sprintpilot_*'],
+} satisfies Config;
