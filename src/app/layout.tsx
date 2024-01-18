62b0ca39f492f2456b3e50a441826978855bf8db
Margus Liinev <margus.liinev@gmail.com>
2024-01-18 21:36:16 +0200

    added metadata, global styles, icons and bg image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import '@/styles/globals.css';
-
+import type { Viewport, Metadata } from 'next';
+import type {} from 'next';
 import { Inter } from 'next/font/google';
 
 const inter = Inter({
@@ -7,10 +8,25 @@ const inter = Inter({
     variable: '--font-sans',
 });
 
-export const metadata = {
+export const viewport: Viewport = {
+    width: 'device-width',
+    initialScale: 1,
+    themeColor: [
+        { media: '(prefers-color-scheme: light)', color: '#fafafa' },
+        { media: '(prefers-color-scheme: dark)', color: '#18181b' },
+    ],
+};
+
+export const metadata: Metadata = {
     title: 'Sprintpilot',
     description: 'Sprintpilot is a project management tool for software development teams.',
-    icons: [{ rel: 'icon', url: '/favicon.ico' }],
+    keywords: ['sprintpilot', 'project management', 'software development', 'sprint planning', 'sprint board', 'agile', 'scrum', 'kanban'],
+    icons: {
+        icon: [{ url: '/favicon.ico' }, { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' }, { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' }],
+        apple: [{ url: '/apple-touch-icon.png', sizes: '180x180' }],
+    },
+    robots: { index: true, follow: true, nosnippet: false, 'max-image-preview': 'large' },
+    manifest: '/site.webmanifest',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
+import Image from 'next/image';
+
 export default function HomePage() {
     return (
-        <main className='flex min-h-screen flex-col items-center justify-center bg-zinc-900 text-white'>
+        <main className='flex min-h-screen flex-col items-center justify-center'>
             <div className='container flex flex-col items-center justify-center gap-12 px-4 py-16 '>
-                <h1 className='text-5xl font-extrabold text-white sm:text-[5rem]'>SprintPilot</h1>
+                <span className='flex items-center gap-4'>
+                    <Image src='/logo-light.svg' width={50} height={50} alt='logo light' />
+                    <h1 className='text-5xl font-extrabold sm:text-[5rem]'>SprintPilot</h1>
+                </span>
                 <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8'>
-                    <div className='flex max-w-sm flex-col gap-4 rounded-xl bg-white/10 p-12 text-white hover:bg-white/20 text-2xl'>Project Management</div>
-                    <div className='flex max-w-sm flex-col gap-4 rounded-xl bg-white/10 p-12 text-white hover:bg-white/20 text-2xl'>Real time collaboration</div>
+                    <div className='flex max-w-sm flex-col gap-4 rounded-xl font-semibold bg-zinc-300 text-zinc-900 p-12 text-2xl'>Project Management</div>
+                    <div className='flex max-w-sm flex-col gap-4 rounded-xl font-semibold bg-zinc-300 text-zinc-900 p-12 text-2xl'>Real time collaboration</div>
                 </div>
             </div>
         </main>
