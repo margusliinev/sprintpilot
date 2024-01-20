613db5acb6986c9eb22734a96fb769a5dbde9cb8
Margus Liinev <margus.liinev@gmail.com>
2024-01-20 02:31:01 +0200

    switched to node version 20.11.0

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,10 @@
 export default function Index() {
     return (
         <main className='h-screen w-screen grid place-items-center'>
-            <h1 className='text-5xl font-extrabold sm:text-[5rem]'>SprintPilot</h1>
+            <div className='flex items-center'>
+                <img src='/apple-touch-icon.png' alt='SprintPilot Logo' width={90} height={90} />
+                <h1 className='text-5xl font-extrabold sm:text-[5rem]'>SprintPilot</h1>
+            </div>
         </main>
     );
 }
