7665c769188dd212b2fdf841298532c0978f8033
Margus Liinv <margus.liinev@gmail.com>
2024-08-17 17:10:54 +0300

    add docker-compose file, env file, zod packages

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
 
+const PORT = process.env.PORT;
+if (!PORT) throw new Error('PORT is not defined');
+
 const app = new Hono();
 
 app.get('/', (c) => {
     return c.text('Hello Hono!');
 });
 
-const port = 3000;
-console.log(`Server is running on port ${port}`);
+console.log('Server is running on port', PORT);
 
-serve({
-    fetch: app.fetch,
-    port
-});
+serve({ fetch: app.fetch, port: Number(PORT) });
