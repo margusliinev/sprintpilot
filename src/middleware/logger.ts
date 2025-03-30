ce0c56aeac86005e33ebcbab12557e36eb03f2ef
Margus Liinev <margus.liinev@pipedrive.com>
2025-03-30 10:27:27 +0300

    improve logging more

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -5,21 +5,30 @@ import { env } from '../helpers/env';
 import { Context } from 'hono';
 import winston from 'winston';
 
-const { combine, timestamp, colorize, printf, errors, json } = winston.format;
+const { combine, timestamp, colorize, printf, json } = winston.format;
+
+const upperCase = winston.format((info) => {
+    info.level = info.level.toUpperCase();
+    return info;
+});
 
 const devFormat = combine(
-    timestamp(),
+    timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    upperCase(),
     colorize(),
-    errors({ stack: true }),
     printf(({ timestamp, level, message, errors, stack }) => {
-        const formattedErrors = errors ? `- ${Object.entries(errors).map(([_, value]) => value)}` : '';
-        const formattedStack = stack ? `\n${stack}` : '';
+        const messageWithErrors = errors
+            ? `${message} - ${Object.entries(errors)
+                  .map(([_, value]) => value)
+                  .join(' || ')}`
+            : message;
+        const formattedStack = stack ? `\n\n${stack}\n` : '';
 
-        return `${timestamp} ${level} - ${message} ${formattedErrors} ${formattedStack}`;
+        return `${timestamp} ${level} - ${messageWithErrors} ${formattedStack}`;
     }),
 );
 
-const liveFormat = combine(timestamp(), errors({ stack: true }), json());
+const liveFormat = combine(timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), json());
 
 const loggerInstance = winston.createLogger({
     format: env.ENV === 'dev' ? devFormat : liveFormat,
@@ -54,45 +63,40 @@ export class Logger {
         const errors = error instanceof CustomException ? error.errors : undefined;
         const level = status >= 500 ? 'error' : status >= 400 ? 'warn' : 'info';
         const stack = status >= 500 ? error.stack : undefined;
+        const meta = { errors, stack };
 
-        this.log(level, message, { errors, stack });
+        this.log(level, message, meta);
     }
 
-    warn(message: string) {
-        this.log('warn', message);
+    warn(message: string, meta?: Record<string, unknown>) {
+        this.log('warn', message, meta);
     }
 
-    info(message: string) {
-        this.log('info', message);
+    info(message: string, meta?: Record<string, unknown>) {
+        this.log('info', message, meta);
     }
 
     logRequest(start: number, end: number) {
-        const requestId = this.context.get('requestId');
         const userAgent = this.context.req.header('User-Agent');
         const referrer = this.context.req.header('Referrer');
         const method = this.context.req.method;
         const path = this.context.req.path;
         const duration = (end - start).toFixed(3);
         const status = this.context.res.status;
-        const sessionId = this.context.get('session')?.id;
-        const userId = this.context.get('user')?.id;
 
         const logData = {
-            requestId,
             userAgent,
             referrer,
             method,
             path,
             duration,
             status,
-            sessionId,
-            userId,
         };
 
         const level = status >= 500 ? 'error' : status >= 400 ? 'warn' : 'info';
         const message = `${method} ${path} - ${status} ${duration}ms`;
 
-        loggerInstance.log(level, message, logData);
+        this.log(level, message, logData);
     }
 }
 
