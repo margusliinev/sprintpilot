6777ec532e4a2f51354a5bb5f1b69e6794bad467
Margus Liinev <margus.liinev@gmail.com>
2025-06-08 09:12:39 +0300

    remove .DS_Store file, add to gitignore

--- src/crons/index.ts
+++ src/crons/index.ts
@@ -1,13 +1,13 @@
-import { scheduleJob } from './registry';
-import { Patterns } from './pattners';
+// import { scheduleJob } from './registry';
+// import { Patterns } from './pattners';
 
 export async function runCrons() {
     try {
-        scheduleJob({
-            name: 'log-every-5-seconds',
-            pattern: Patterns.EVERY_5_SECONDS,
-            handler: () => console.log(`(${new Date().toUTCString()}) This will run every five seconds`),
-        });
+        // scheduleJob({
+        //     name: 'log-every-5-seconds',
+        //     pattern: Patterns.EVERY_5_SECONDS,
+        //     handler: () => console.log('This will run every five seconds'),
+        // });
         console.info('✅ Cronjobs setup completed');
     } catch (error) {
         console.error('❌ Cronjobs setup failed');
