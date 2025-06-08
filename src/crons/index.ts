646c069f75320a25cd68733fd06839f4f1b36c8a
Margus Liinev <margus.liinev@gmail.com>
2025-06-08 17:36:57 +0300

    disable crons

--- src/crons/index.ts
+++ src/crons/index.ts
@@ -1,15 +1,15 @@
 // import { scheduleJob } from './registry';
 // import { Patterns } from './pattners';
 
-export async function runCrons() {
-    try {
-        // scheduleJob({
-        //     name: 'log-every-5-seconds',
-        //     pattern: Patterns.EVERY_5_SECONDS,
-        //     handler: () => console.log('This will run every five seconds'),
-        // });
-        console.info('✅ Cronjobs setup completed');
-    } catch (error) {
-        console.error('❌ Cronjobs setup failed');
-    }
-}
+// export async function runCrons() {
+//     try {
+//         scheduleJob({
+//             name: 'log-every-5-seconds',
+//             pattern: Patterns.EVERY_5_SECONDS,
+//             handler: () => console.log('This will run every five seconds'),
+//         });
+//         console.info('✅ Cronjobs setup completed');
+//     } catch (error) {
+//         console.error('❌ Cronjobs setup failed');
+//     }
+// }
