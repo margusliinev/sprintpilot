01b1a344420eb3c979e3039a5d9649e6e13df99a
Margus Liinev <margus.liinev@gmail.com>
2025-06-14 17:47:38 +0300

    update vite config

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,20 +1,15 @@
 import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import { defineConfig } from 'vite';
-import { resolve } from 'node:path';
 import viteReact from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 export default defineConfig({
     plugins: [tanstackRouter({ autoCodeSplitting: true }), viteReact(), tailwindcss()],
-    resolve: { alias: { '@': resolve(__dirname, './src') } },
+    resolve: { alias: { '@': path.resolve(__dirname, './src') } },
     build: { outDir: 'build' },
     server: {
-        proxy: {
-            '/api': {
-                target: 'http://localhost:3000',
-                changeOrigin: true,
-            },
-        },
+        proxy: { '/api': { target: 'http://localhost:3000', changeOrigin: true } },
         host: 'localhost',
         port: 3001,
     },
